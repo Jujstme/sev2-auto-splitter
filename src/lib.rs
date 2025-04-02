ddf6f8efc8f4b3f10384847296da1ec83083009c
Jujstme <just.tribe@gmail.com>
2025-04-02 13:59:58 +0200

    Refactored code in idiomatic rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,186 +1,242 @@
 #![no_std]
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![allow(static_mut_refs)]
+#![warn(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::perf,
+    clippy::style,
+    clippy::undocumented_unsafe_blocks,
+    rust_2018_idioms
+)]
 
-use asr::{future::{next_tick, retry}, settings::{Gui, Map}, Process};
-use core::{str};
+use asr::{
+    Address, Process,
+    file_format::pe,
+    future::{next_tick, retry},
+    settings::Gui,
+    string::ArrayCString,
+    time::Duration,
+    timer::{self, TimerState},
+    watcher::Watcher,
+};
 
 asr::async_main!(stable);
 asr::panic_handler!();
 
-#[derive(Gui)]
-struct Settings {
-    #[default = true]
-    Full_game_run: bool,
-    #[default = false]
-    Individual_level: bool,
-    #[default = false]
-    Slow_PC_mode: bool
-}
-
-struct Addr {
-    startAddress: u32,
-    loadAddress: u32,
-    splashAddress: u32,
-    levelAddress: u32,
-    bulletCamAddress: u32,
-    objectiveAddress: u32,
-    mcAddress: u32
-}
-
-impl Addr {
-    fn original() -> Self {
-        Self {
-            startAddress: 0x689FE2,
-            loadAddress: 0x67FC38,
-            splashAddress: 0x653B40,
-            levelAddress: 0x685F31,
-            bulletCamAddress: 0x65B917,
-            objectiveAddress: 0x656F3C,
-            mcAddress: 0x689FD2
-        }
-    }
-    
-    fn remastered() -> Self {
-        Self {
-            startAddress: 0x799A77,
-            loadAddress: 0x774FE3,
-            splashAddress: 0x74C670,
-            levelAddress: 0x7CFC7D,
-            bulletCamAddress: 0x76DD17,
-            objectiveAddress: 0x7CF568,
-            mcAddress: 0x799A63
-        }
-    }
-}
+const PROCESS_NAMES: &[&str] = &["SniperEliteV2.exe", "SEV2_Remastered.exe"];
 
 async fn main() {
     let mut settings = Settings::register();
-    let mut map = Map::load();
 
-    let mut tickToggled = false;
-    asr::set_tick_rate(60.0);
-
-    let mut startByte: u8 = 0;
-    
-    let mut loadByte: u8 = 0;
-    
-    static mut splashByte: u8 = 0;
-    let mut oldSplash: u8 = 0;
-    
-    static mut levelStr: &str = "";
-    static mut levelArray: [u8; 2] = [0; 2];
-    static mut oldLevel: [u8; 2] = [0; 2];
-    
-    let mut bulletCamByte: u8 = 0;
-    let mut objectiveByte: u8 = 0;
-    
-    let mut mcByte: u8 = 0;
-    
-    
-    let mut baseAddress = asr::Address::new(0);
-    let mut addrStruct = Addr::original();
     loop {
-        let process = retry(|| {["SniperEliteV2.exe", "SEV2_Remastered.exe", "MainThread"].into_iter().find_map(Process::attach)}).await; // MainThread = Wine/Proton
+        // Hook to the target process
+        let (process_name, process) = retry(|| {
+            PROCESS_NAMES
+                .iter()
+                .find_map(|&name| Some((name, Process::attach(name)?)))
+        })
+        .await;
 
-        process.until_closes(async {
-            if let Some((base, moduleSize)) = ["SniperEliteV2.exe", "SEV2_Remastered.exe"].into_iter().find_map (|exe|
-            Some((process.get_module_address(exe).ok()?,
-            process.get_module_size(exe).ok()?)))
-            {
-                baseAddress = base;
-                if moduleSize == 18169856 || moduleSize == 512000 { //512000 = Remastered Wine/Proton
-                    addrStruct = Addr::remastered();
-                }
-            }
+        process
+            .until_closes(async {
+                // Once the target has been found and attached to, set up some default watchers
+                let mut watchers = Watchers::default();
 
-            unsafe {
-                let mut start = || {
-                    startByte = process.read::<u8>(baseAddress + addrStruct.startAddress).unwrap_or(0);
-                    if startByte == 1 {
-                        asr::timer::start();
-                    }
-                };
-
-                let mut isLoading = || {
-                    loadByte = process.read::<u8>(baseAddress + addrStruct.loadAddress).unwrap_or(1); 
-                    splashByte = process.read::<u8>(baseAddress + addrStruct.splashAddress).unwrap_or(1);
-                    if loadByte == 0 || splashByte == 0 {
-                        asr::timer::pause_game_time();
-                    }
-                    else {
-                        asr::timer::resume_game_time();
-                    }
-                };
-
-                let levelSplit = || {
-                    if levelArray != oldLevel && !levelStr.is_empty() && levelStr != "nu" && levelStr != "Tu" {
-                        asr::timer::split();
-                    }
-                };
-
-                let mut lastSplit = || {
-                    bulletCamByte = process.read::<u8>(baseAddress + addrStruct.bulletCamAddress).unwrap_or(0); 
-                    objectiveByte = process.read::<u8>(baseAddress + addrStruct.objectiveAddress).unwrap_or(0);
-                
-                    if levelStr == "Br" && bulletCamByte == 1 && objectiveByte == 3 {
-                        asr::timer::split();
-                    }
-                };
-
-                let mut individualLvl = || {
-                    mcByte = process.read::<u8>(baseAddress + addrStruct.mcAddress).unwrap_or(0);
-                
-                    if mcByte == 1 {
-                        asr::timer::split();
-                    }
-                    if splashByte != oldSplash {
-                        oldSplash = splashByte;
-                    }
-                    if (splashByte == 1 && oldSplash == 0) && levelStr != "nu" {
-                        asr::timer::start();
-                    }
-                };
+                // Perform memory scanning to look for the addresses we need
+                let addresses = Memory::init(&process, process_name).await;
 
                 loop {
+                    // Splitting logic. Adapted from OG LiveSplit:
+                    // Order of execution
+                    // 1. update() will always be run first. There are no conditions on the execution of this action.
+                    // 2. If the timer is currently either running or paused, then the isLoading, gameTime, and reset actions will be run.
+                    // 3. If reset does not return true, then the split action will be run.
+                    // 4. If the timer is currently not running (and not paused), then the start action will be run.
                     settings.update();
 
-                    if settings.Full_game_run && settings.Individual_level {
-                        map.store();
+                    if watchers.slow_pc_mode != settings.slow_pc_mode {
+                        asr::set_tick_rate(match settings.slow_pc_mode {
+                            true => 60.0,
+                            false => 120.0,
+                        });
+
+                        watchers.slow_pc_mode = settings.slow_pc_mode;
                     }
 
-                    if settings.Slow_PC_mode && !tickToggled {
-                        asr::set_tick_rate(30.0);
-                        map = Map::load();
-                        tickToggled = true;
-                    }
-                    else if !settings.Slow_PC_mode && tickToggled {
-                        asr::set_tick_rate(60.0);
-                        map = Map::load();
-                        tickToggled = false;
+                    update_loop(&process, &addresses, &mut watchers);
+
+                    if [TimerState::Running, TimerState::Paused].contains(&timer::state()) {
+                        match is_loading(&watchers, &settings) {
+                            Some(true) => timer::pause_game_time(),
+                            Some(false) => timer::resume_game_time(),
+                            _ => (),
+                        }
+
+                        match game_time(&watchers, &settings, &addresses) {
+                            Some(x) => timer::set_game_time(x),
+                            _ => (),
+                        }
+
+                        match reset(&watchers, &settings) {
+                            true => timer::reset(),
+                            _ => match split(&watchers, &settings) {
+                                true => timer::split(),
+                                _ => (),
+                            },
+                        }
                     }
 
-                    process.read_into_slice(baseAddress + addrStruct.levelAddress, &mut levelArray).unwrap_or_default();
-                    levelStr = str::from_utf8(&levelArray).unwrap_or("").split('\0').next().unwrap_or("");
+                    if timer::state().eq(&TimerState::NotRunning) && start(&watchers, &settings) {
+                        timer::start();
+                        timer::pause_game_time();
 
-                    if settings.Full_game_run {
-                        //let start_time = asr::time_util::Instant::now();
-                        start();
-                        //let end_time = start_time.elapsed();
-                        levelSplit();
-                        lastSplit();
-                        //asr::print_message(&alloc::format!("Tick time: {:?}", end_time));
+                        match is_loading(&watchers, &settings) {
+                            Some(true) => timer::pause_game_time(),
+                            Some(false) => timer::resume_game_time(),
+                            _ => (),
+                        }
                     }
-                    if settings.Individual_level {
-                        individualLvl();
-                    }
-                    isLoading();
 
-                    oldLevel = levelArray;
                     next_tick().await;
                 }
-            }
-        }).await;
+            })
+            .await;
     }
+}
+
+#[derive(Gui)]
+struct Settings {
+    /// IL mode
+    #[default = false]
+    individual_level: bool,
+    /// Slow PC mode (reduces the refresh rate from 120hz to 60hz)
+    #[default = false]
+    slow_pc_mode: bool,
+}
+
+struct Memory {
+    start: Address,
+    load: Address,
+    splash: Address,
+    level: Address,
+    bullet: Address,
+    objective: Address,
+    mc: Address,
+}
+
+impl Memory {
+    async fn init(process: &Process, main_module_name: &str) -> Self {
+        let main_module_base = retry(|| process.get_module_address(main_module_name)).await;
+        let main_module_size = retry(|| pe::read_size_of_image(process, main_module_base)).await;
+
+        // let is_64_bit = retry(|| MachineType::pointer_size(MachineType::read(process, main_module_base)?)).await == PointerSize::Bit64;
+
+        match main_module_size {
+            0x1154000 => Self {
+                // remastered
+                start: main_module_base + 0x799A77,
+                load: main_module_base + 0x774FE3,
+                splash: main_module_base + 0x74C670,
+                level: main_module_base + 0x7CFC7D,
+                bullet: main_module_base + 0x76DD17,
+                objective: main_module_base + 0x7CF568,
+                mc: main_module_base + 0x799A63,
+            },
+            _ => Self {
+                // OG?
+                start: main_module_base + 0x689FE2,
+                load: main_module_base + 0x67FC38,
+                splash: main_module_base + 0x653B40,
+                level: main_module_base + 0x685F31,
+                bullet: main_module_base + 0x65B917,
+                objective: main_module_base + 0x656F3C,
+                mc: main_module_base + 0x689FD2,
+            },
+        }
+    }
+}
+
+#[derive(Default)]
+struct Watchers {
+    slow_pc_mode: bool,
+    start_byte: Watcher<u8>,
+    load_byte: Watcher<u8>,
+    splash_byte: Watcher<u8>,
+    level: Watcher<ArrayCString<2>>,
+    bullet_cam: Watcher<u8>,
+    objective: Watcher<u8>,
+    mc: Watcher<u8>,
+}
+
+fn update_loop(process: &Process, memory: &Memory, watchers: &mut Watchers) {
+    watchers
+        .start_byte
+        .update_infallible(process.read(memory.start).unwrap_or_default());
+
+    watchers
+        .load_byte
+        .update_infallible(process.read(memory.load).unwrap_or_else(|_| 1));
+    watchers
+        .splash_byte
+        .update_infallible(process.read(memory.splash).unwrap_or_else(|_| 1));
+
+    watchers
+        .bullet_cam
+        .update_infallible(process.read(memory.bullet).unwrap_or_default());
+    watchers
+        .objective
+        .update_infallible(process.read(memory.objective).unwrap_or_default());
+    watchers
+        .mc
+        .update_infallible(process.read(memory.mc).unwrap_or_default());
+
+    watchers
+        .level
+        .update_infallible(process.read(memory.level).unwrap_or_default());
+}
+
+fn start(watchers: &Watchers, settings: &Settings) -> bool {
+    match settings.individual_level {
+        true => watchers.splash_byte.pair.is_some_and(|val| {
+            val.changed_from_to(&0, &1)
+                && watchers
+                    .level
+                    .pair
+                    .is_some_and(|val| !val.current.matches("nu"))
+        }),
+        false => watchers
+            .start_byte
+            .pair
+            .is_some_and(|val| val.changed_to(&1)),
+    }
+}
+
+fn is_loading(watchers: &Watchers, _settings: &Settings) -> Option<bool> {
+    Some(watchers.load_byte.pair?.current == 1 && watchers.splash_byte.pair?.current == 1)
+}
+
+fn split(watchers: &Watchers, settings: &Settings) -> bool {
+    match settings.individual_level {
+        true => watchers.mc.pair.is_some_and(|val| val.changed_to(&1)),
+        false => {
+            watchers.level.pair.is_some_and(|val| {
+                val.changed()
+                    && !val.current.is_empty()
+                    && !val.current.matches("nu")
+                    && !val.matches("Tu")
+            }) || (watchers
+                .level
+                .pair
+                .is_some_and(|val| val.current.matches("Br"))
+                && watchers.bullet_cam.pair.is_some_and(|val| val.current == 1)
+                && watchers.objective.pair.is_some_and(|val| val.current == 3))
+        }
+    }
+}
+
+fn game_time(_watchers: &Watchers, _settings: &Settings, _addresses: &Memory) -> Option<Duration> {
+    None
+}
+
+fn reset(_watchers: &Watchers, _settings: &Settings) -> bool {
+    false
 }
